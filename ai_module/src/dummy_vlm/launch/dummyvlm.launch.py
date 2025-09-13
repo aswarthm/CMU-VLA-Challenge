81acecce5950fb36e94daf120ed6a87408ebfe91
Aswarth M <m.aswarthreddy@gmail.com>
2025-09-14 02:25:56 +0530

    start from launch_module

--- ai_module/src/dummy_vlm/launch/dummyvlm.launch.py
+++ ai_module/src/dummy_vlm/launch/dummyvlm.launch.py
@@ -1,41 +1,20 @@
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.actions import IncludeLaunchDescription ,DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution,LaunchConfiguration, PythonExpression
-import os
-from ament_index_python.packages import get_package_share_directory,get_package_prefix
+
 
 def generate_launch_description():
-    # share_dir = get_package_share_directory('hb_task2b')
-    # pkg_sim_world = get_package_share_directory('hb_world')
-    # pkg_sim_bot = get_package_share_directory('hb_bot')
-
-
-     
-    # world = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(pkg_sim_world, 'launch', 'world.launch.py'),
-    #     )
-    # )
 
     ros1_bridge = Node(
         package='ros1_bridge',
-        executable='dynamic_bridge'
+        executable='dynamic_bridge',
+        arguments=['--bridge-all-1to2-topics']
     )
 
     goalPub = Node(
-            package='dummy_vlm',
-            executable='sub.py',
-        )
-    # feedback = Node(
-    #         package='hb_task2b',
-    #         executable='feedback.py',
-    #     )        
-    
-    
+        package='dummy_vlm',
+        executable='sub.py',
+    )
+
     return LaunchDescription([
         # world,
         # ros1_bridge,
@@ -43,4 +22,4 @@ def generate_launch_description():
         # controller1,
         # controller2,
         # controller3,
-        ])
+    ])
--- launch_module.sh
+++ launch_module.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sleep 3
+cd ai_module
+cd genai-server && python3.9 app.py &
+ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics > /dev/null 2>&1 &
+colcon build && source install/local_setup.bash && ros2 launch dummy_vlm dummyvlm.launch.py 
 
-source ai_module/devel/setup.bash
-
-# source /opt/ros/foxy/setup.bash
-
-roslaunch dummy_vlm dummy_vlm.launch
