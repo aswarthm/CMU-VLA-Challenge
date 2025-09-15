6e6a3e06919c7a5f418c09a3571b13ee04d0916c
Aswarth M <m.aswarthreddy@gmail.com>
2025-09-16 01:31:21 +0530

    bridge all topics

--- launch_module.sh
+++ launch_module.sh
@@ -2,6 +2,6 @@
 
 cd ai_module
 cd genai-server && python3.9 app.py &
-ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics > /dev/null 2>&1 &
+ros2 run ros1_bridge dynamic_bridge --bridge-all-topics & #> /dev/null 2>&1 &
 colcon build && source install/local_setup.bash && ros2 launch dummy_vlm dummyvlm.launch.py 
 
